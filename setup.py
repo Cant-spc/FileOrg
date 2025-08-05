240447f776c9918623e45e7d7458af5005955b06
Tanish Gupta <tanishgupta12389@gmail.com>
2025-08-05 10:55:51 +0530

    version 1.1 with yaml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="filefix",
     author="Tanish Gupta",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         'typer>=0.4.1',
