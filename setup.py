d42ebcbc90564b145071dad2d1ac0e030294c4d2
Tanish Gupta <tanishgupta12389@gmail.com>
2025-09-30 08:12:49 +0530

    moded

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="filefix",
-    author="",
+    author="Tanish Gupta",
     version="0.1.2",
     packages=find_packages(),
     install_requires=[
